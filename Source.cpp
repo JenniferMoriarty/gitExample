d6fb732175ce24eef1d5b4ef8ea65fe196cc151c
Jennifer Moriarty <jmoriarty78@gmail.com>
2022-01-09 10:51:12 -0700

    initial commit

---
+++ Source.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main() {
+
+	for (int i = 0; i < 80; i += 10)
+		cout << i << " ";
+
+	cout << endl<<endl;
+
+	for (int i = 20; i > -20; i--)
+		cout << i << " ";
+
+	cout << endl << endl;
+	for (int i = 3; i < 2000; i *= 5)
+		cout << i << " ";
+
+	cout << endl << endl;
+}
